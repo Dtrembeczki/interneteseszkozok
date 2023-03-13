f72c10b758231ceb5031be8e2226f7f508b569a9
Dtrembeczki <tremb.d98@gmail.com>
2023-03-13 18:48:56 +0100

    fname, lname, dateofbirth hozzaadva

--- includes/insertUsr.inc.php
+++ includes/insertUsr.inc.php
@@ -2,22 +2,26 @@
     include '../src/user/User.php';
     include '../config/Database.php';
 
-    $db = new Database();
+    $db = new Database("iek");
     $conn = $db->connection();
 
 
     if (isset($_POST['signup'])){
         
         $name = mysqli_real_escape_string($conn, $_POST['name']);
+        $firstname = mysqli_real_escape_string($conn, $_POST['fname']);
+        $lastname = mysqli_real_escape_string($conn, $_POST['lname']);
         $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+        $pwd_check = mysqli_real_escape_string($conn, $_POST['pwdcheck']);
         $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $dateofbirth = $_POST['dateofbirth'];
         $sex = mysqli_real_escape_string($conn, $_POST['sex']);
 
-        $usr = new User($name, $pwd, $email, $sex);
+        $usr = new User($name, $firstname,$lastname, $pwd, $email, $sex, $dateofbirth);
         if (!$usr->insertUsr($conn)){
-            header('Location: ../signup.php?msg=success');
+            header('Location: ../pages/signup.php?msg=success');
             exit;
         }else{
-            header('Location: ../signup.php?msg=sql');
+            header('Location: ../pages/signup.php?msg=sql');
         }
     }
--- pages/signup.php
+++ pages/signup.php
@@ -10,13 +10,32 @@
 
         <!--Bootsrtap sigup form-->
         <form class="col-sm-6 mx-auto p-5 bg-body mt-5" action="../includes/insertUsr.inc.php" method="post">
+
+            <!--username-->
             <div class="row mb-3">
-                <label for="inputEmail3" class="col-sm-3 col-form-label">Felhasználó név</label>
+                <label for="name3" class="col-sm-3 col-form-label">Felhasználó név</label>
                 <div class="col-sm-9">
-                    <input type="text" class="form-control" id="inputEmail3" name="name">
+                    <input type="text" class="form-control" id="name3" name="name">
                 </div>
             </div>
 
+            <!--first name-->
+            <div class="row mb-3">
+                <label for="fname3" class="col-sm-3 col-form-label">Keresztnév</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="fname3" name="fname">
+                </div>
+            </div>
+
+            <!--last name-->
+            <div class="row mb-3">
+                <label for="lname3" class="col-sm-3 col-form-label">Vezetéknév</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="lname3" name="lname">
+                </div>
+            </div>
+
+            <!--email-->
             <div class="row mb-3">
                 <label for="inputEmail3" class="col-sm-3 col-form-label">Email</label>
                 <div class="col-sm-9">
@@ -24,6 +43,7 @@
                 </div>
             </div>
 
+            <!--PWD-->
             <div class="row mb-3">
                 <label for="inputPassword3" class="col-sm-3 col-form-label">Jelszó</label>
                 <div class="col-sm-9">
@@ -31,6 +51,23 @@
                 </div>
             </div>
 
+            <!--PWD again-->
+            <div class="row mb-3">
+                <label for="inputPassword3check" class="col-sm-3 col-form-label">Jelszó újra</label>
+                <div class="col-sm-9">
+                    <input type="password" class="form-control" id="inputPassword3check" name="pwdcheck">
+                </div>
+            </div>
+
+            <!--Date of birth-->
+            <div class="row mb-3">
+                <label for="inputPassword3" class="col-sm-3 col-form-label">Születési idő</label>
+                <div class="col-sm-9">
+                    <input type="date" class="form-control" id="inputPassword3" name="dateofbirth">
+                </div>
+            </div>
+
+            <!--sex-->
             <fieldset class="row mb-3">
 
                 <legend class="col-form-label col-sm-3 pt-0">Nemed:</legend>
@@ -54,13 +91,22 @@
                         <label class="form-check-label" for="gridRadios2">
                             Egyéb/nem nyilatkozom
                         </label>
-                    </div> 
+                    </div>
+
                 </div>
 
             </fieldset>
 
-            <div class="row mb-3">
-                <div class="col-sm-3">
+            <!--aszf-->
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="aszf" id="gridCheckbox2" value="ok">
+                <label class="form-check-label" for="gridCheckbox2">
+                    Elolvastam az Általános Szerződési Feltételeket.
+                </label>
+            </div>
+
+            <div class="row mx-3">
+                <div class="col-sm-3 mt-2">
                     <button type="submit" class="btn btn-primary" name="signup">Regisztráció</button>
                 </div>
                 <div class="col-sm-9 d-flex justify-content-end">
