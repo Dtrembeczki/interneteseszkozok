d2dbc9a270a999a986e6ff57bb26fe89c610d8b3
David Trembeczki <79248865+Dtrembeczki@users.noreply.github.com>
2023-03-31 21:38:17 +0200

    Delete src directory

--- src/js/validate.js
+++
@@ -1,9 +0,0 @@
-const username = document.getElementById("name3");
-const fname = document.getElementById("fname3");
-const lname = document.getElementById("lname3");
-
-username.addEventListener("blur", function(){
-    if ( isEmpty(this.value) ){
-        username.style.borderColor = "red";
-    }
-});
